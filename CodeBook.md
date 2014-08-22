72a68cd358e2d3688845a34b50cc8b4f87ecbc36
xwarzbek <xwarzbek@gmail.com>
2014-08-22 08:18:41 -0700

    Class assignment, basics in place
    Clean data, see Codebook. Readme as markdown, would suggest as text, and
    the text version of a tidy dataset with mean, std of activity variables
    from Human Activity Research.

--- CodeBook.md
+++ CodeBook.md
@@ -1,26 +1,72 @@
 ---
-output: pdf_document
+output: html_document
 ---
-+Data Cleaning
-++ Raw Data
-+++ Source
+#Data Cleaning  
+
+A course project for Coursera "Getting and Cleaning Data" from Johns Hopkins, as part of tha Data Scientist suite
+
+****
+
+## Raw Data
+
+### Source
 For more information about this dataset contact: activityrecognition@smartlab.ws
 
-+++ Structure
-+++ License
-Use of this dataset in publications must be acknowledged by referencing the following publication [1] 
+The Coursera source for this is
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
-[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+The permanent repository is at UC Irvine Machine learning:
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphone
 
-This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited.
+### Structure
+The .tar file expands to four files and two subfolders, test and train. Under each is a directory "Intertial Signals" which we will ignore.
 
-Jorge L. Reyes-Ortiz, Alessandro Ghio, Luca Oneto, Davide Anguita. November 2012.
+* 'README.txt'
+* 'features_info.txt': Summary information about the variables 
+* 'features.txt': List of all features.
+* 'activity_labels.txt': factors (in our usage) for activity.
 
-++ Intended features of the Tidy Data set
+#### In the directories
 
-++ Variables and Values
+* 'X_{dir}.txt': Data
+* 'y_{dir}.txt': Labels
+* 'subject_{dir}.txt': Each row identifies the particpant (from 1to 30). 
 
-++Narrative of run_analyses.R
+The test and train datasets were distinguished by participant
+
+### License
+> Use of this dataset in publications must be acknowledged by 
+> referencing the following publication:
+
+> Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and
+> Jorge L. Reyes-Ortiz. 
+> *Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine.*
+> International Workshop of Ambient Assisted Living (**IWAAL 2012**).  Vitoria-Gasteiz, Spain. Dec 2012
+
+> *This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited.*
+
+*Jorge L. Reyes-Ortiz, Alessandro Ghio, Luca Oneto, Davide Anguita. November 2012.*
+
+
+## Variables and Values
+
+The original measurements (X.txt) were presented as 561 normalized (i.e. unitless) values, combining events (time) and frequencies (fft, Fast Fourier Transform). From these, 86 were extracted based on the name (features.txt), declaring them to be mean and standard deviations of the measurements, by participant (Y.txt). 
+The oringial files are not readable from a Windows 32-bit "notepad" application, but are straightforwardly handled by the R "read.table."
+
+## Narrative of run_analyses.R
+
+This version of "run_analyses" assumes that you have downloaded and unpacked the source archive from either site above. The name of the directory containing the README is placed in the environment as "LOCAL_DATA."  The files are read in to intermediate values.  
+
+A series of name substitutions are made on the variable names, removing parentheses, commas, and other punctuation.  To reiterate the twostyles of measurements, the "t" and "f" prefixes are expanded out. There is an extraneous ")" at the 556th member of the list, which needed explicit removal.
+
+To better manage memory, explicit intermediate removal and garbage collection are invoked. If memeory was strongly an issues, column selection could be done before building a data frame.  
+Factors are associated from the activity definitions into each test and train preliminary frame, and subject IDs injected in. Just in case some analysis reconstruction may be needed to extract train v test sets, the final column "source" codes this as a numerical factor.
+
+The merged data set is created combining both sets.
+
+Desired measurements are identified by name construction (mean, Mean, and std being indicators) and the "keep" list of columns is generated.  The final tidy set is then this subset.
+
+
 
 
 
--- run_analysis.R
+++ run_analysis.R
@@ -1,31 +1,105 @@
-## The purpose of this project is to demonstrate your ability to collect, work 
-## with, and clean a data set. The goal is to prepare tidy data that can be used 
-## for later analysis. You will be graded by your peers on a series of yes/no 
-## questions related to the project. You will be required to submit: 1) a tidy data 
-## set as described below, 2) a link to a Github repository with your script for 
-## performing the analysis, and 3) a code book that describes the variables
-## called CodeBook.md. You should also include a README.md in the repo with your 
-## scripts. This repo explains how all of the scripts work and how they are 
-## connected.  
-
-## One of the most exciting areas in all of data science right now is wearable computing - see for example this article . Companies like Fitbit, Nike, and Jawbone Up are racing to develop the most advanced algorithms to attract new users. The data linked to from the course website represent data collected from the accelerometers from the Samsung Galaxy S smartphone. A full description is available at the site where the data was obtained: 
-## http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
-## Here are the data for the project: 
-
+## Collect, work with, and clean a data set. 
+## The goal is to prepare "tidy data"
+ 
+## Data Source
 ## https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
 
-## You should create one R script called run_analysis.R that does the following. 
-## Merges the training and the test sets to create one data set.
-## Extracts only the measurements on the mean and standard deviation for each measurement. 
-## Uses descriptive activity names to name the activities in the data set
-## Appropriately labels the data set with descriptive variable names. 
-## Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
-## Good luck!
+## Tasklist: 
+## 1) Merges the training and the test sets to create one data set.
+## 2) Extracts only the measurements on the mean and standard deviation for each measurement. 
+## 3) Uses descriptive activity names
+## 4) Appropriately label the data set with descriptive variable names. 
+## 5) Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+## 
+ 
+## Since this gets posted publicly, I'll put a variable in the environement
+## separate from the analysis file to  manage my local file structure
+
 if (! exists(LOCAL_DATA, inherits = TRUE)) {
   #try to go to internet
-  classUrl <- url("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
-")
-} else {
-  data_test <- read.table(paste(LOCAL_DATA, ))
-}
-  
+  classUrl <- url("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip")
+## other direct download refresh, create LOCAL_DATA
+  } 
+
+## based on directory structure
+dataDir   <- dir(LOCAL_DATA)
+testName  <- paste(LOCAL_DATA, "test/", sep = "")
+testDir   <- dir(testName)
+trainName <- paste(LOCAL_DATA, "train/", sep = "")
+trainDir  <- dir(trainName)
+
+Activities <- read.table(paste(LOCAL_DATA, dataDir[1], sep =""))[,2]
+Measurements <-  read.table(paste(LOCAL_DATA, dataDir[2], sep =""))[,2]  
+Subject.0 <- read.table(paste( testName,  testDir[2], sep =""))
+Subject.1 <- read.table(paste(trainName, trainDir[2], sep =""))
+
+      X.0 <- read.table(paste( testName,   testDir[3], sep =""))
+      X.1 <- read.table(paste(trainName,  trainDir[3], sep =""))
+      Y.0 <- read.table(paste( testName,   testDir[4], sep =""))
+      Y.1 <- read.table(paste(trainName,  trainDir[4], sep =""))
+
+## Let's pretty up the variable names from "Measurements"
+## Rules: ^t = time domain
+##        ^f = Fourier transform
+##         Parentheses are not needed
+##         Dashes are ok
+##         Commas are right out.
+## I find mixed case (camelCase) easier to read than all lower.
+## We will assume that the files are in the correct order
+## This is task #3
+## make the prefixes nicer
+   m.1 <- sub("^t", "time_", sub("^f", "fft_", Measurements))
+## Parens. first remove "()" as it means nothing. Then (is helpful
+## to indicate a variation. The end parenthesis is useless
+##              
+   m.2 <- sub("\\(", "_Of_", sub( ")", "", sub("\\()", "", m.1)))
+##              ^ double slashes to escape the open parenthesis
+## All together, and an extra look for")" to cover [556]
+MeasureColNames <- sub(")", "", sub(",", "-", m.2))
+
+rm(m.2)
+rm(m.1)
+   gc()
+## Cleanup on aisle 1
+## we add a column to track to source, just in case 
+## Subject some from Subject.{0,1}, and we make factors readable 
+## from the Y.N
+
+Activity.0 <- factor(Y.0[,1], label=Activities)
+Activity.1 <- factor(Y.1[,1], label=Activities)
+
+data.f0 <- data.frame(cbind(Subject.0, Activity.0, X.0, 0))
+data.f1 <- data.frame(cbind(Subject.1, Activity.1, X.1, 1))
+## This is task 4
+Names <- c("Subject", "Activity", MeasureColNames, "source")
+names(data.f0) <- Names
+names(data.f1) <- Names
+
+## Now, Task #1. It would be more memory efficient to have removed
+## unneeded columns first, but I wanted to maintain the set as long
+##as possible, so no mismatch would be likely
+
+HAR_tidyALL.df <- rbind(data.f0, data.f1)
+
+## Now, we do task #2, focus on only the columns with "mean" or "std" 
+## in the name. Also, keep the first two, and the last one = 564
+keepColumns <- c( 1, 2, 
+                  sort(c(grep("Mean", MeasureColNames), 
+                      grep("mean", MeasureColNames), 
+                      grep("std", MeasureColNames))
+                    ) +2, 564)
+HAR_tidy.df <- HAR_tidyALL.df[,keepColumns]
+##
+## Final cleanup (putting this into a function would 
+##                make this go away.)
+rm(HAR_tidyALL.df)
+rmlist <- list("Subject.0", "Subject.1", "MeasureColNames")
+rm("keepColumns", list = rmlist)
+gc()
+
+
+return(HAR_tidy.df)
+
+
+
+
